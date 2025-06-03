31131df1af74bb49235e688772d6aef183e753bf
Han <hanc26@gwmail.gwu.edu>
2025-06-03 14:01:38 -0400

    creating logic for adding a listening grid in TicTacToe

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,5 +1,7 @@
 import java.awt.*;
-import javax.swing.JFrame;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
 
 
 public class TicTacToe {
@@ -8,7 +10,7 @@ public class TicTacToe {
 
 
     public TicTacToe() {
-        f = new JFrame();
+        f = new JFrame("Lets Play TicTacToe!");
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -18,6 +20,35 @@ public class TicTacToe {
         setBounds();
         drawGrid();
         playGame();
+    }
+
+    public void setBounds() {
+        f.setBounds(0, 0, 1500, 1000);
+    }
+
+    public void drawGrid() {
+        //create new panel to add grid to
+        JPanel grid = new JPanel();
+
+        grid.setLayout(new GridLayout(3, 3));
+        grid.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+
+        //add button listeners:
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                grid.addMouseListener(new MouseListener() ) {
+                    
+                }
+            }
+        }
+
+
+        f.add(grid);
+    }
+
+    public void playGame() {
+
     }
 
 }
